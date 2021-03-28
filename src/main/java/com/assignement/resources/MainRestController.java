ec8ef4335c58608efffa9f5d321b3a34be1c08b6
charafeddine boughriba <charafeddine.boughriba@orange.com>
2021-03-28 10:22:24 +0200

    adding swagger and logger

--- src/main/java/com/assignement/resources/MainRestController.java
+++ src/main/java/com/assignement/resources/MainRestController.java
@@ -15,29 +15,37 @@ import com.assignement.entities.Account;
 import com.assignement.entities.AccountCreationBody;
 import com.assignement.entities.Customer;
 import com.assignement.entities.Transaction;
-import com.assignement.exceptions.DAOException;
 import com.assignement.services.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class MainRestController {
 
+	Logger logger = LoggerFactory.getLogger(MainRestController.class);
+	
 	@Autowired
 	CustomerService customerService;
 
 	@GetMapping("/getCustomer")
 	Customer getCustomer(@RequestParam Integer id) throws Exception {
-		if (id == null)
+		if (id == null) {
+			logger.error("cutsomer Id cannot be null");
 			throw new Exception("cutsomer Id cannot be null");
+		}
 		Customer res = this.customerService.findCustomerById(id);
+		logger.info("found customer for id : " + id + " : "  + res);
 		return res;
 	}
 
 	@PostMapping("/createAccount")
 	Customer createAccount(@RequestBody AccountCreationBody body) throws Exception {
-		if (body.getCustomerId() == null)
+		if (body.getCustomerId() == null) {
+			logger.error("cutsomer Id cannot be null");
 			throw new Exception("cutsomer Id cannot be null");
+		}
 		Customer res = this.customerService.findCustomerById(body.getCustomerId());
 		Account account = new Account("title", "desc", new Date(), false);
 		if (body.getBalance() == null)
@@ -48,6 +56,7 @@ public class MainRestController {
 		res.addAccount(account);
 		account.setCustomer(res);
 		customerService.saveCustomer(res);
+		logger.info("created new account and added new transaction with provided balance");
 		return res;
 	}
 }
